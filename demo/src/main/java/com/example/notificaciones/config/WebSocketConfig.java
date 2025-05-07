cf1fd927a747214dbb7a111c87d7e7cb71d89eba
cristicn2024 <cristi_cn_2004@outlook.com>
2025-05-07 03:42:39 -0700

    bro NO SE PORQUE AGARRA EL PUERTO 8080
    welp, no esta declarado en ningun lado el 8080.

---
+++ demo/src/main/java/com/example/notificaciones/config/WebSocketConfig.java
@@ -0,0 +1,52 @@
+package com.example.notificaciones.config;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author crist
+ */
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+       registry.addEndpoint("/ws-notificaciones")
+        .setAllowedOrigins("*"); 
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+
+    // Esto habilita CORS para cualquier ruta REST también
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ demo/src/main/java/com/example/notificaciones/model/Notificacion.java
@@ -0,0 +1,17 @@
+package com.example.notificaciones.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author crist
+ */
+import lombok.Data;
+
+@Data
+public class Notificacion {
+    private String message;
+}
