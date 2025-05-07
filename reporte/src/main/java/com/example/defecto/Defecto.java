8387334368dc2c4e130b04c2d92d41ca83879185
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-05-07 17:15:14 -0600

    Acomodo de proyectos
    si no sirve, le haré revert

---
+++ reporte/src/main/java/com/example/defecto/Defecto.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.defecto;
+
+import com.example.tipodedefecto.TipoDeDefecto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Ramos
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "defecto")
+public class Defecto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idDefecto")
+    private Long idDefecto;
+
+    @Column(name = "costo", nullable = false)
+    private Float costo;
+
+    @Column(name = "detalles", nullable = false)
+    private String detalles;
+
+    @Column(name = "cantidad_piezas", nullable = false)
+    private int cantidad_piezas;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_defecto", nullable = false)
+    private TipoDeDefecto tipoDefecto;
+
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "idPieza", referencedColumnName = "idPieza")
+//    private Pieza pieza;
+}
--- reporte/src/main/java/com/example/reportes/ReporteControlador.java
+++ reporte/src/main/java/com/example/reportes/ReporteControlador.java
@@ -6,7 +6,6 @@ package com.example.reportes;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
