5af63be192dba204054f716d6f85c2e0ce1e0193
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-05-10 15:51:01 -0600

    Cleanup pt2

--- target/classes/static/js/script.js
+++
@@ -1,6 +0,0 @@
-fetch('api/v1/usuarios')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.mensaje);
-            document.getElementById("mensaje").innerText = data[0].fullName;
-        });
